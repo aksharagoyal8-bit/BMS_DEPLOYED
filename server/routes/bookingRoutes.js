3d8604617314e6cfacb7e74a953cb97a13aa25a0
aksharagoyal8 <aksharagoyal8@gmail.com>
2026-06-02 22:37:56 +0530

    zas

--- server/routes/bookingRoutes.js
+++ server/routes/bookingRoutes.js
@@ -80,7 +80,7 @@ router.post("/book-show", authMiddleware, async (req, res) => {
         populatedBooking.user.email,
         {
           name: populatedBooking.user.name,
-          movie: populatedBooking.show.movie.movieName,
+          movie: populatedBooking.show.movie.title,
           theatre: populatedBooking.show.theatre.name,
           date: populatedBooking.show.date,
           time: populatedBooking.show.time,
