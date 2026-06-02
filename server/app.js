dac36ed33b100234d325b5de2da68046fa96080c
aksharagoyal8 <aksharagoyal8@gmail.com>
2026-06-02 19:37:48 +0530

    some changes

--- server/app.js
+++ server/app.js
@@ -1,45 +1,45 @@
-
 require('dotenv').config(); 
 const express = require('express');
-const cors=require("cors");
-const path=require("path");
-const rateLimit= require("express-rate-limit");
-const helmet=require ("helmet");
+const cors = require("cors");
+const path = require("path");
+const rateLimit = require("express-rate-limit");
+const helmet = require("helmet");
 const connectDB = require('./config/db');
-const userRoute=require('./routes/userRoutes');
-const movieRoute =require("./routes/movieRoutes");
-const theatreRoutes=require("./routes/theatreRoutes");
-const showRoute=require("./routes/showRoutes");
-const bookRoute=require("./routes/bookingRoutes");
+const userRoute = require('./routes/userRoutes');
+const movieRoute = require("./routes/movieRoutes");
+const theatreRoutes = require("./routes/theatreRoutes");
+const showRoute = require("./routes/showRoutes");
+const bookRoute = require("./routes/bookingRoutes");
 
-app.use(express.static(path.join(__dirname, 'build') ));
-app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname, 'build','index.html'));
-})
+const app = express();
+const PORT = process.env.PORT || 8080;
 
 connectDB(process.env.DB_URL);
 
-const app = express();
-const PORT = 8080;
-
 app.use(helmet());
-
-const apiLimiter=rateLimit({
-    windowMs:15*60*1000,
-    max:100,
-    message:"Too many requests from this IP,Please try again later after 15 minutes."
-})
-
 app.use(express.json());
 app.use(cors());
-app.use("/api/",apiLimiter);
-app.use("/api/users",userRoute);
-app.use("/api/movie",movieRoute);
-app.use("/api/theatre",theatreRoutes);
-app.use("/api/show",showRoute);
-app.use("/api/booking",bookRoute);
+
+const apiLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+    message: "Too many requests from this IP, Please try again later after 15 minutes."
+});
 
 
-app.listen(PORT,()=>{
+app.use("/api/", apiLimiter);
+app.use("/api/users", userRoute);
+app.use("/api/movie", movieRoute);
+app.use("/api/theatre", theatreRoutes);
+app.use("/api/show", showRoute);
+app.use("/api/booking", bookRoute);
+
+
+app.use(express.static(path.join(__dirname, 'build')));
+app.get('*path', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
+
+app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
-})
+});
